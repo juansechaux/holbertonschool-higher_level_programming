313426cd52b600e5253456816c8031f452629e47
juansechaux <juansechaux@hotmail.com>
2023-09-28 08:03:37 -0700

    Task 6 program that prints all possible different combinations of two digits, use .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,13 +3,13 @@ d = 0
 i = 1
 while d < 9:
     if d == 8 and i == 9:
-        print(f"{d}{i}")
+        print("{}{}".format(d, i))
         break
     elif i > d and i < 9:
-        print(f"{d}{i}, ", end="")
+        print("{}{}, ".format(d, i), end="")
         i = i + 1
     elif i == 9:
-        print(f"{d}{i}, ", end="")
+        print("{}{}, ".format(d, i), end="")
         i = 0
         d = d + 1
     else:
