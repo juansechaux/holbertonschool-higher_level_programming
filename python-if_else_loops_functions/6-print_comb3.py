f1ecdfdc63ab6320a7d1fe6c08423c1a369681d2
juansechaux <juansechaux@hotmail.com>
2023-09-28 08:00:41 -0700

    Task 6 program that prints all possible different combinations of two digits

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+d = 0
+i = 1
+while d < 9:
+    if d == 8 and i == 9:
+        print(f"{d}{i}")
+        break
+    elif i > d and i < 9:
+        print(f"{d}{i}, ", end="")
+        i = i + 1
+    elif i == 9:
+        print(f"{d}{i}, ", end="")
+        i = 0
+        d = d + 1
+    else:
+        i = i + 1
