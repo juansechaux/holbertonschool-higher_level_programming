c0d8e42f3a0d426adf2ad97c2561eb98008851b3
juansechaux <juansechaux@hotmail.com>
2023-09-29 14:22:49 -0500

    Task 4 program that prints all the names defined by the compiled module hidden_4.pyc

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import hidden_4
+if __name__ == "__main__":
+    for name in dir(hidden_4):
+        if name[0] == "_" and name[1] == "_":
+            continue
+        else:
+            print("{}".format(name))
