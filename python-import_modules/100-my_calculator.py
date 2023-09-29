664d633a8fe2003797fa462fb5129d0d5393836e
juansechaux <juansechaux@hotmail.com>
2023-09-29 15:55:15 -0500

    Task 6 program that imports all functions from the file calculator_1.py and handles basic operations

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,16 +5,16 @@ if __name__ == "__main__":
     long = len(argv)
     if long != 4:
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
-        exit (1)
+        exit(1)
     else:
         if argv[2] == "+":
-            print("{} + {} = {}".format(argv[1], argv[3], add(argv[1], argv[3])))
+            print("{} + {} = {}".format(argv[1], argv[3], add(int(argv[1]), int(argv[3]))))
         elif argv[2] == "-":
-            print("{} - {} = {}".format(argv[1], argv[3], sub(argv[1], argv[3])))
+            print("{} - {} = {}".format(argv[1], argv[3], sub(int(argv[1]), int(argv[3]))))
         elif argv[2] == "*":
-            print("{} * {} = {}".format(argv[1], argv[3], mul(argv[1], argv[3])))
+            print("{} * {} = {}".format(argv[1], argv[3], mul(int(argv[1]), int(argv[3]))))
         elif argv[2] == "/":
-            print("{} / {} = {}".format(argv[1], argv[3], div(argv[1], argv[3])))
+            print("{} / {} = {}".format(argv[1], argv[3], div(int(argv[1]), int(argv[3]))))
         else:
             print("{}".format("Unknown operator. Available operators: +, -, * and /"))
-            exit (1)
+            exit(1)
