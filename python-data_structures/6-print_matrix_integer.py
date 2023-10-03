8e59eb29b85a5d7b1602776fd8d9d00816b528fd
juansechaux <juansechaux@hotmail.com>
2023-10-03 14:47:42 -0500

    Task 6 function that prints a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        empy = 0
+        empty = 0
         for i in matrix:
             long = len(i)
             cont = 1
             for c in i:
                 if long == cont:
                     print("{:d}".format(c))
-                    empy += 1
+                    empty += 1
                 else:
                     print("{:d} ".format(c), end=(""))
-                    empy += 1
+                    empty += 1
                 cont += 1
-        if empy == 0:
+        if empty == 0:
             print("{}".format(""))
