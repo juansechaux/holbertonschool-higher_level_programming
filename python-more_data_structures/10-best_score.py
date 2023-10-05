c92da1d173b5481fab0e4d978892eca54ba115ec
juansechaux <juansechaux@hotmail.com>
2023-10-05 15:30:37 -0500

    Task 10 function that returns a key with the biggest integer value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        big_key = sorted(a_dictionary, key=sorted, reverse=True)
+        big_key = sorted(a_dictionary, key=a_dictionary.get, reverse=True)
         return big_key[0]
     else:
         return None
