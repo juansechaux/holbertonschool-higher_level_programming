4cdb659706fecf0c776a3ae86575f6f7a31a9c25
juansechaux <juansechaux@hotmail.com>
2023-10-05 14:42:05 -0500

    Task 8 function that deletes a key in a dictionary

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    key_exist = key in a_dictionary
-    if key_exist == False:
+    if key in a_dictionary:
+        del a_dictionary[key]
         return a_dictionary
     else:
-        del a_dictionary[key]
         return a_dictionary
