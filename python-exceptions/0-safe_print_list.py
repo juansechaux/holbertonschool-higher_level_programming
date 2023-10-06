0e8b8f43533ffbd6329381f957732911ad7d1b74
juansechaux <juansechaux@hotmail.com>
2023-10-06 12:55:02 -0500

    Task 0 function that prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
     except IndexError:
-        return (my_list[i - 1])
+        return (i)
     else:
-        return (my_list[i])
+        return (i + 1)
     finally:
         print("")
