7298995fb47e4b74c0832a4a083ffe86776e28bd
juansechaux <juansechaux@hotmail.com>
2023-10-10 16:25:13 -0500

    Task 3 class Square that defines a square by: (based on 2-square.py) Public instance method: def area(self): that returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """This class has private instance attribute: size"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                print("size must be >= 0", end="")
+                raise ValueError
+        else:
+            print("size must be an integer", end="")
+            raise TypeError
+
+    def area(self):
+        return (self.__size * self.__size)
