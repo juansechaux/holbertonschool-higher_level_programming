a38a9d6e28a0da0ab8738801ec5f788f65bbd76f
juansechaux <juansechaux@hotmail.com>
2023-10-10 15:20:44 -0500

    Task 1 class Square that defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """This class has private instance attribute: size"""
+    def __init__(self, size):
+        self.__size = size
