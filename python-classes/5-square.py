25ab5aa9b2d7047119928aa7341fcc8467b3e410
juansechaux <juansechaux@hotmail.com>
2023-10-10 17:33:41 -0500

    Task 4 class Square that defines a square by: (based on 3-square.py) - Private instance attribute: size - property def size(self): to retrieve it - property setter def size(self, value): to set it - Public instance method: def my_print(self): that prints in stdout the square with the character # - if size is equal to 0, print an empty line

---
+++ python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """This class has private instance attribute: size"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                print("size must be >= 0", end="")
+                raise ValueError
+        else:
+            print("size must be an integer", end="")
+            raise TypeError
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        for colum in range(0, self.__size):
+            for rows in range(0, self.__size + 1):
+                if rows == self.__size:
+                    print("#")
+                else:
+                    print("#", end="")
