954ec5efce0b1761cc1bcd45d8d7105fb70b404c
juansechaux <juansechaux@hotmail.com>
2023-10-11 15:31:59 -0500

    Task 6 class Square that defines a square by: (based on 5-square.py) - Private instance attribute: size - property def size(self): to retrieve it - property setter def size(self, value): to set it - Public instance method: def my_print(self): that prints in stdout the square with the character # - if size is equal to 0, print an empty line - Private instance attribute: position - property def position(self): to retrieve it - property setter def position(self, value): to set it - position should be use by using space - Don’t fill lines by spaces when position[1] > 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -28,18 +28,16 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if len(value) == 2:
-            if isinstance(value[0], int) and isinstance(value[1], int):
-                if value[0] >= 0 and value[1] >= 0:
-                    self.__position = (value[0], value[1])
-                else:
-                    print("position must be a tuple of 2 positive integers")
-                    raise TypeError
-            else:
-                print("position must be a tuple of 2 positive integers")
-                raise TypeError
-        else:
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int and type(value[1]) is not int:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
+        if value[0] >= 0 and value[1] >= 0:
+            self.__position = (value[0], value[1])
+        else:
+            print("position must be a tuple of 2 positive integers", end="")
+            raise TypeError
 
     def area(self):
         return (self.__size * self.__size)
