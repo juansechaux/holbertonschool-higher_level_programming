4aaa878a83c5c9245a44528bb8833ffde7d462c2
juansechaux <juansechaux@hotmail.com>
2023-10-11 15:18:25 -0500

    Task 6 class Square that defines a square by: (based on 5-square.py) - Private instance attribute: size - property def size(self): to retrieve it - property setter def size(self, value): to set it - Public instance method: def my_print(self): that prints in stdout the square with the character # - if size is equal to 0, print an empty line - Private instance attribute: position - property def position(self): to retrieve it - property setter def position(self, value): to set it - position should be use by using space - Don’t fill lines by spaces when position[1] > 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,10 +34,10 @@ class Square:
                     self.__position = (value[0], value[1])
                 else:
                     print("position must be a tuple of 2 positive integers")
-                    raise TypeError(end="")
+                    raise TypeError
             else:
                 print("position must be a tuple of 2 positive integers")
-                raise TypeError(end="")
+                raise TypeError
         else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
