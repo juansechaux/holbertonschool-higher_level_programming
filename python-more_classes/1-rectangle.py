fc7d07f86bb0e71ba284b9c2525f62eefa82bf8e
juansechaux <juansechaux@hotmail.com>
2023-10-12 11:39:55 -0500

    Task 1 class Rectangle that defines a rectangle by: (based on 0-rectangle.py) - Private instance attribute: width - Private instance attribute: height - Instantiation with optional width and height: def __init__(self, width=0, height=0):

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -33,3 +33,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height)
