31cc702a672cd66b1903e5db75fc5fe622d639f7
juansechaux <juansechaux@hotmail.com>
2023-10-12 14:49:36 -0500

    Task 5 class Rectangle that defines a rectangle by: (based on 4-rectangle.py) - Print the message Bye rectangle... (... being 3 dots not ellipsis) when an instance of Rectangle is deleted

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""This is an class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """This class actualy pass"""
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        new_str = ""
+        if self.__width == 0 or self.__height == 0:
+            return new_str
+        else:
+            for colum in range(0, self.__height):
+                for rows in range(0, self.__width):
+                    new_str += "#"
+                if colum < self.__height - 1:
+                    new_str += "\n"
+            return new_str
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+        del (self)
