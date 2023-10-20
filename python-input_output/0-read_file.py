b93ff8292722e1fca0c878c2c1803e4b259f093f
juansechaux <juansechaux@hotmail.com>
2023-10-20 15:46:17 -0500

    Task 0 - function that reads a text file (UTF8) and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Module that reads a text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
+    """Function that reads a text file (UTF8) and prints it to stdout
+    """
     with open(filename) as file:
         print(file.read(), end="")
