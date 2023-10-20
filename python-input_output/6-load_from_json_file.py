b352271b5f29d794fd791eb8367e597e720ec232
juansechaux <juansechaux@hotmail.com>
2023-10-20 17:45:56 -0500

    Task 6 - function that creates an Object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that creates an Object from a “JSON file”.
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file,
+    """
+    with open(filename, mode="r") as file:
+        return json.load(file)
