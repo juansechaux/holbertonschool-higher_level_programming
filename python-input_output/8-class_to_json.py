f9c008df2c42cdf0a44ffd567e86c2811e3fd675
juansechaux <juansechaux@hotmail.com>
2023-10-20 19:44:31 -0500

    Task 8 function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""that returns the dictionary
+description with simple data structure
+"""
+import json
+
+def class_to_json(obj):
+    """function that returns the dictionary
+    description with simple data structure
+    """
+    return obj.__dict__
