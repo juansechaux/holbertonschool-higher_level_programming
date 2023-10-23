63f25b2fa8cc92c67c057e3c7c3a9d86dc49a3ef
juansechaux <juansechaux@hotmail.com>
2023-10-23 10:10:51 -0500

    Task 12 - function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n - n will be always an integer

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" function def pascal_triangle(n):
+""" Module def pascal_triangle(n):
     that returns a list of lists of integers
     representing the Pascals triangle of n
 """
 
 
 def pascal_triangle(n):
+    """ function def pascal_triangle(n):
+    that returns a list of lists of integers
+    representing the Pascals triangle of n
+    """
     c_list = []
     if n <= 0:
         return c_list
