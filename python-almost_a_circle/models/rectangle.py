cf92db54eda9140f7f9197b389f4755ad8637ce4
juansechaux <juansechaux@hotmail.com>
2023-10-25 12:19:00 -0500

    Task 7 - Update the class Rectangle by improving the public method def display(self): to print in stdout the Rectangle instance with the character # by taking care of x and y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,11 +71,9 @@ class Rectangle(Base):
     def display(self):
         """function that print the rectangle with "#"
         """
-        for y_col in range(self.__y):
-            print("")
+        print("\n" * self.__y, end="")
         for col in range(self.__height):
-            print(" " * self.__x, end="")
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Magic method str,
