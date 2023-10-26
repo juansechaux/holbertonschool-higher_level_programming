2268c6dcdd764dde0e70fd1ea985d7b242ed4e59
juansechaux <juansechaux@hotmail.com>
2023-10-26 11:28:12 -0500

    Task 15 - Update the class Base by adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries - If list_dictionaries is None or empty, return the string: []

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,6 +20,9 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """public method, that returns a list of
+        a dictionary representation.
+        """
         new_dict = []
         if list_dictionaries is None or list_dictionaries is ():
             return new_dict
