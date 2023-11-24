e7023014af24c3f59c995c1ec18352ca5f4a9030
juansechaux <juansechaux@hotmail.com>
2023-11-24 16:09:00 -0500

    Task 4, script that creates the table id_not_null on your MySQL server

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- script that creates the table force_name on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
