a52c6ea3a40e1e7f323e40f42b5116005c4c0e9e
juansechaux <juansechaux@hotmail.com>
2023-11-24 16:48:32 -0500

    Task 7, script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- cript that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     id INT AUTO_INCREMENT PRIMARY KEY,
