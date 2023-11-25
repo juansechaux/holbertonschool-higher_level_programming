d3a91544b93d80605c281fdc1c665fc2a0e038b8
juansechaux <juansechaux@hotmail.com>
2023-11-25 10:39:07 -0500

    Task 9, Write a script that lists all cities contained in the database hbtn_0d_usa

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -6,6 +6,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT cities.id, cities.name, states.name
 FROM cities
-LEFT JOIN states
+JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id ASC;
