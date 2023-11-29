278c023cbb189be4cbb038181b23ed24a698bc94
juansechaux <juansechaux@hotmail.com>
2023-11-28 23:48:45 -0500

    Task 4, Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,10 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT cities.id, cities.name,states.name FROM cities JOIN states ON cities.state_id = states.id")
+    cursor.execute("SELECT cities.id, cities.name, states.name "
+                   "FROM cities "
+                   "JOIN states ON cities.state_id = states.id")
+
     for cities in cursor.fetchall():
         print(cities)
 
