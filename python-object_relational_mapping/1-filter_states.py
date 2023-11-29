eadcd3ada3a9628ef9df780722ae34294b2aed17
juansechaux <juansechaux@hotmail.com>
2023-11-28 21:10:13 -0500

    Task 1, Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     password = argumentos[2]
     database = argumentos[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password,
-                         db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
 
     cursor = db.cursor()
 
