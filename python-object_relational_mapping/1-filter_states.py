a7ffa51940e741d1062a71db87e9e9257e057d12
juansechaux <juansechaux@hotmail.com>
2023-11-28 20:34:54 -0500

    Task 1, Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script to list states from database hbtn_0e_0_usa."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """Get arguments  from command-line"""
+    argumentos = sys.argv
+
+    username = argumentos[1]
+    password = argumentos[2]
+    database = argumentos[3]
+
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password,
+                         db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY 'state.id' ASC")
+    for states in cursor.fetchall():
+        print(states)
+
+    cursor.close()
+    db.close()
