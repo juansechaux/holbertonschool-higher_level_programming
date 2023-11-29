22feab61a186c0eb74ba3225ada2d49cd4b4bc5c
juansechaux <juansechaux@hotmail.com>
2023-11-28 22:00:29 -0500

    Task 3, SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A script to list states from database hbtn_0e_0_usa."""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """Get arguments  from command-line"""
+    argumentos = sys.argv
+
+    username = argumentos[1]
+    password = argumentos[2]
+    database = argumentos[3]
+    state_name = argumentos[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states")
+    for states in cursor.fetchall():
+        if states[1] == state_name:
+            print('({}, \'{}\')'.format(states[0], states[1]))
+
+    cursor.close()
+    db.close()
