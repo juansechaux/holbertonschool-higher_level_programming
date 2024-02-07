33f939d18e479d4b52dc482ec451f07124ef72e4
juansechaux <juansechaux@hotmail.com>
2024-02-07 15:11:22 -0500

    Project JavaScript DOM manipulation

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,17 +1,16 @@
-const divHello = document.getElementById('hello');
-
 fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
   .then(response => {
     if (!response.ok) {
-        throw new Error(`HTTP error, Status: ${response.status}`)
+      throw new Error(`HTTP error, Status: ${response.status}`)
     }
     return response.json();
   })
   .then(data => {
+    const divHello = document.getElementById('hello');
     const newP = document.createElement('p');
     newP.textContent = data.hello;
     divHello.appendChild(newP);
   })
   .catch(error => {
-    console.error('Error fetching data:', error);
-  })
+    console.error(`Error fetching data: ${error}`);
+  });
